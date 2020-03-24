773fba4f7d346523f76e06d84cc6ed4162f6c83c
aakashDeveloper <ahanda205@gmail.com>
2020-03-24 17:21:06 +0000

    a

---
+++ first.js
@@ -0,0 +1,59 @@
+string "gerg" 'dgd'  "sf" '111' 'true'
+Number 7867867 8789.878 87867868
+Boolean true/false
+
+Es5
+Es6
+Ecma Script
+
+
+//Es5//
+var a = 10
+var b = "hi"
+var c = true
+var d = "11"
+var e = "false"
+undefined
+typeof(a)
+"number"
+typeof(b)
+"string"
+typeof(c)
+"boolean"
+typeof(d)
+"string"
+typeof(e)
+"string"
+
+//Number operations//
+var a = 10
+var b = 20
+a+b
+30
+a-b
+-10
+a*b
+200
+a/b
+0.5
+4%2
+
+4%2
+0
+4%3
+1
+4%4
+0
+
+2%4
+3%4
+
+2%4
+3%4
+3
+2%4
+2
+3%4
+3
+100%101
+100
---
+++ second.js
@@ -0,0 +1,113 @@
+var a = 10 //assignment
+a == 10 // comparision
+a===10//compare  data & datatype
+
+var a = 10
+var b = "10"
+
+a==b
+a===b
+a == b
+true
+a ===b 
+false
+
+true == "true"
+false
+true === "true"
+false
+
+////////
+var a= "hi "
+var b = "bie"
+
+
+var a= "hi "
+var b = "bie"
+undefined
+a+b
+"hi bie"
+var a = "10"
+undefined
+var b = "5"
+undefined
+a+b
+"105"
+var a= "hi "
+var b = "bie"
+undefined
+a-b
+NaN
+a*b
+NaN
+a/b
+NaN
+a%b
+NaN
+var c = 10
+undefined
+a+c
+"hi 10"
+d = true
+true
+a+d
+"hi true"
+true+true
+2
+true-false
+1
+true-true
+0
+
+true = 1
+false = 0
+
+
+
+string+string = string
+string+number = string
+number+string = string
+number+number = number
+Boolean+Boolean = number
+
+"5"+5+5
+"555"
+5+5+"5"
+"105"
+5+"5"+5
+"555"
+
+
+BODMAS
+
+"5"+5+5-2
+
+5+5+"5"-2
+
+5+"5"+5-2
+
+
+"5"+5+5-2
+553
+5+5+"5"-2
+103
+5+"5"+5-2
+553
+'a'-'a'
+NaN
+'1'-'1'
+0
+'1'+'1'
+'11'
+'1'-'a'
+NaN
+'2'-'3'
+-1b
+'2'*'3'
+6
+'2'/'3'
+0.6666666666666666
+'1'-'a'
+NaN
+'223'+'22'
+"22322"
