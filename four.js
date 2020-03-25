4ba51c75300134e80539156871ee3ad0c038d683
aakashDeveloper <ahanda205@gmail.com>
2020-03-25 17:06:07 +0000

    A

---
+++ four.js
@@ -0,0 +1,8 @@
+var city = "london"
+undefined
+city.toUpperCase()
+"LONDON"
+var city = "London"
+undefined
+city.toLowerCase()
+"london"
---
+++ third.js
@@ -0,0 +1,51 @@
+console.log("hiiiii"); //To dubug code
+alert('Form submitted') //For pop up message
+
+//
+confirm("Do You want to leave")
+
+confirm("Do You want to leave")
+true
+confirm("Do You want to leave")
+false
+var a = confirm("Do You want to leave")
+undefined
+a
+true
+prompt("What is your age")
+"26"
+var age = prompt("What is your age")
+undefined
+age
+"10"
+
+
+var a = prompt('First Number')
+var b = prompt('Second number')
+var output = parseFloat(a)+parseFloat(b)
+alert('Sum is '+output)
+"1"+"1"
+'11'
+parseInt(a)+parseInt(b)
+
+/*
+"1"+"1" = 11
+parseInt("1")+parseInt("1") =2
+1+1="11"
+*/
+
+parseInt("1")
+1
+parseInt("aa")
+NaN
+parseInt("1a")
+1
+parseInt("135a")
+135
+parseInt("a135a")
+NaN
+parseInt("a135")
+NaN
+parseInt("135a11")
+135
+
