67aee1d85938056a2e9d62bafc06262de42cf4df
aakashDeveloper <ahanda205@gmail.com>
2020-03-27 17:17:26 +0000

    a

---
+++ seven.js
@@ -0,0 +1,88 @@
+var a = 10
+
+if(a){
+    do something
+}else{
+    do something
+}
+
+
+var a = 10;
+if(a%2==0){
+    console.log('Number is even')
+}else if(a%3==0){
+    console.log('Number is divisible by 3')
+}
+else{
+    console.log('Number of odd')
+}
+
+
+
+var a = 10;
+if(a%2==0){
+    console.log('Number is even')
+}else if(a%3==0){
+    console.log('Number is divisible by 3')
+}
+else{
+    console.log('Number of odd')
+}
+
+
+
+var a = 12
+if(a%2 ==0){
+    console.log('Number is even')
+}
+if(a%3==0){
+    console.log('Number is divisible by 3')
+}
+else{
+    console.log('Number of odd')
+}
+
+
+var a= 9;
+if(a%2 ==0){
+    console.log('Number is even')
+}
+else if(a%3==0 || a%5==0){
+    console.log("i am in block")
+    if(a%3==0){
+        console.log('Number is divisible by 3')
+    }else{
+        console.log('Number is divisible by 5')
+    }
+}
+else{
+    console.log("I don't know")
+}
+
+
+///Ternery 
+var a =10
+
+a>10?"hi":"bie"
+var a =10
+
+a>10?"hi":"bie"
+"bie"
+var a =10
+
+a==10?"hi":"bie"
+"hi"
+var a =10
+
+a>10?a-1:a+1
+11
+var a =10
+
+a==10?a-1:a+1
+9
+
+//////
+Luck Game
+> Generate one number between 1 to 20
+> take one input from user
+> if both match say user win else user loose 
---
+++ six.js
@@ -0,0 +1,47 @@
+var a = 10
+a>12
+a<12
+a==10
+var a = 10
+undefined
+a>12
+false
+a<12
+true
+a == 10
+true
+a === 10
+true
+a != 10
+false
+a !== 10
+false
+var a = true
+
+var a = !b
+
+
+var a = 10
+!a
+
+
+var a = 10
+!a
+false
+var a = 0
+!a
+true
+var a = -1
+undefined
+!a
+false
+
+
+////////
+true || true   = true
+true || false  = true
+false||false   = false
+
+true && true = true
+true || false  = false
+false||false   = false
